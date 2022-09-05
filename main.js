4f12b7ee2cb10fc84e134b555f91318fdd9d0267
cbsecoachingcentre <112490773+cbsecoachingcentre@users.noreply.github.com>
2022-09-05 11:42:26 +0530

    Change

--- main.js
+++ main.js
@@ -184,7 +184,7 @@ let foot = document.getElementById('b-example-divider');
 
 foot.innerHTML=` <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
               <div class="col-md-4 d-flex align-items-center">
-                <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                <a href="/.com" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
 
 
                   <img class="bi" width="30" height="24" src="/img/1.png">
